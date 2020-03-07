e9a69a00d9ce0b018950b0be08dee00cfdd9ee2a
sumit kumar <pksumitkumar98@gmail.com>
2020-03-07 22:01:25 +0530

    make

---
+++ public/javascripts/ap.js
@@ -0,0 +1,10 @@
+var scrollmsg = document.querySelector("#messages");
+var scrollbtn = document.querySelector("#btn");
+scrollbtn.addEventListener("click", function() {
+
+    var h = scrollmsg.clientHeight
+
+    console.log(h)
+
+
+})
